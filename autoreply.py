2d2798520f864cb00b6f7a9303d0de27172fe745
Sinku <sininoir@gmail.com>
2021-11-24 19:56:11 +0330

    Update autoreply

--- autoreply.py
+++ autoreply.py
@@ -1,12 +1,16 @@
 import tweepy
 
-def check_mentions(api, since_id):
+def check_mentions(api, since_id, quacker):
     print('reading mentions...')
     since = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline, 
                                 since_id=since_id).items():
+        print(f'{tweet.user.name} replied:{tweet.text}')
         since = max(tweet.id, since)
-        api.create_favorite(tweet.id)
-        api.update_status(status="type duck message here", 
-                in_reply_to_status_id=tweet.id)
-        return since
+        try:
+            api.create_favorite(tweet.id)
+            api.update_status(status="type duck message here",
+                    in_reply_to_status_id=tweet.id)
+        except:
+            print('ERROR: can\'t like or tweet! help me!')
+    return since
