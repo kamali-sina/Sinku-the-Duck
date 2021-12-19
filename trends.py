21d3e297088101c9e3524f7478b701ebd3d3c008
Sinku <sininoir@gmail.com>
2021-12-19 20:31:24 +0330

    Change depricated function name

--- trends.py
+++ trends.py
@@ -1,5 +1,5 @@
 import tweepy
 
-def get_latest_trend(api):
-    trends_result = api.trends_place(1)
-    return trends_result[0]['trends'][0]
+def get_latest_trend(api : tweepy.API):
+    trends_result = api.get_place_trends(1)
+    return trends_result[0]['trends'][1]
